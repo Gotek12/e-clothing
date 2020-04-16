28ec642f3f821f1767696a451c874b574edae2c9
Wojtek L <wladyga@gmail.com>
2020-04-16 11:37:04 +0200

    Fix routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
+import {Route, Switch} from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
-    <div className="App">
-      <HomePage/>
+    <div>
+      {/* <HomePage/> */}
+      {/* exact -> this path must be exactly / , it's means when we dont have it and we have ex. / and we change to /wtf we have page / and add to page / elements from /wtf*/}
+      {/* switcch is special construct to give us control in switching pathes and rendering data */}
+      <Switch>
+        <Route exact path = "/" component = {HomePage}/>
+        <Route path = "/hats" component = {HatsPage}/>
+        <Route path = "/hats/:id" component = {HatsPage}/>
+      </Switch>
     </div>
   );
 }
